772309129c30780ff20c2f9b6164e3c9276a9199
Andrew Galitskiy <103642883+nesamurai17@users.noreply.github.com>
2025-04-21 00:37:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+let currentEnergy = 30;
+let balance = 1000000;
+let usdtBalance = 100;
+
+function updateUI() {
+  const energyBar = document.getElementById("energy-bar");
+  const energyPercent = document.getElementById("energy-percent");
+  energyBar.style.width = `${currentEnergy}%`;
+  energyPercent.textContent = `${currentEnergy}%`;
+  document.getElementById("balance").textContent = balance.toLocaleString();
+  document.getElementById("usdt-balance").textContent = usdtBalance.toFixed(2);
+}
+
+function showEnergyModal(requiredEnergy) {
+  if (currentEnergy < requiredEnergy) {
+    document.getElementById(
+      "requiredEnergyText"
+    ).textContent = `Требуется ${requiredEnergy}% энергии. Пополнить сейчас?`;
+    document.getElementById("energyModal").style.display = "flex";
+  } else {
+    startTask(requiredEnergy);
+  }
+}
+
+function buyWithCoins() {
+  const cost = requiredEnergy * 10000;
+  if (balance >= cost) {
+    balance -= cost;
+    currentEnergy = 100;
+    closeModal();
+    updateUI();
+  } else {
+    alert("Не достаточно коинов!");
+  }
+}
+
+// document.getElementById('close-webapp-btn').addEventListener('click', function() {
+//     // Проверяем, открыто ли в Telegram WebApp
+//     if (window.Telegram && Telegram.WebApp) {
+//       // Закрываем WebApp
+//       Telegram.WebApp.close();
+//     } else {
+//       // Альтернативное поведение для других браузеров
+//       window.history.back();
+//       // Или можно показать сообщение
+//       alert("Эта функция работает только в Telegram");
+//     }
+//   });
+
+function closeModal(){
+  document.getElementById("energyModal").style.display = "none";
+}
+
+function startTask(energyCost) {
+  currentEnergy -= energyCost;
+  updateUI();
+  alert("Task started!");
+}
+
+document.addEventListener("DOMContentLoaded", updateUI);
